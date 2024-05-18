86c657b4fc29a34faf77015c73864cdac2bf6b20
SHUSHU010829 <shushu90829@gmail.com>
2024-05-18 17:34:52 +0800

    next project init

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Shuyuan Chuang',
+  description: 'All about Shuyuan Chuang.',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang='en'>
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
