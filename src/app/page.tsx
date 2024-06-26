2350403bf92ea3a51f35ec1eac0b15ac2dc9025c
SHUSHU010829 <shushu90829@gmail.com>
2024-06-26 10:28:12 +0800

    fix: 維修中畫面

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className='flex min-h-screen w-full items-center justify-center bg-[#f3f3f3] p-10 font-sans'>
       <div className='flex flex-col items-center gap-5'>
-        <div className='text-2xl font-bold'>網站維修中...⸜(๑˙ᵕ ˙๑)⸝</div>
+        <div className='text-2xl font-bold'>網站重新維修中...⸜(๑˙ᵕ ˙๑)⸝</div>
         <div className='flex justify-center gap-1'>
           <div className='cursor-pointer p-2 duration-300 hover:text-stone-500'>
             <a href='mailto: shuyuan010829@gmail.com'>
