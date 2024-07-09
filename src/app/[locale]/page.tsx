f73845bf37665547f0a6169d6a99162e20fb344f
SHUSHU010829 <shushu90829@gmail.com>
2024-07-09 13:58:45 +0800

    feat: add magic ui

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,3 +1,4 @@
+import GradualSpacing from '@/components/magicui/gradual-spacing';
 import { Github, Linkedin, Mail } from 'lucide-react';
 import { useTranslations } from 'next-intl';
 
@@ -7,7 +8,10 @@ export default function IndexPage() {
   return (
     <main className='flex min-h-screen w-full items-center justify-center bg-[#f3f3f3] p-10 font-sans'>
       <div className='flex flex-col items-center gap-5'>
-        <div className='text-2xl font-bold'>{t('title')}</div>
+        <GradualSpacing
+          className='text-center font-playFairDisplay text-xl font-bold tracking-[-0.1em]  text-black dark:text-white md:text-2xl'
+          text={t('title')}
+        />
         <div className='flex justify-center gap-1'>
           <div className='cursor-pointer p-2 duration-300 hover:text-stone-500'>
             <a href='mailto: shuyuan010829@gmail.com'>
