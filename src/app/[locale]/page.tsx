f36a05317de3d8d0ed541dcb19ce6ef7fae72422
SHUSHU010829 <shushu90829@gmail.com>
2024-07-09 14:31:26 +0800

    feat: see resume btn

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,9 +1,12 @@
+import GetStartedButton from '@/components/animata/getStartedButtton';
 import GradualSpacing from '@/components/magicui/gradual-spacing';
 import { Github, Linkedin, Mail } from 'lucide-react';
 import { useTranslations } from 'next-intl';
+import Link from 'next/link';
 
-export default function IndexPage() {
+export default function IndexPage({ params }: { params: { locale: string } }) {
   const t = useTranslations('Index');
+  const { locale } = params;
 
   return (
     <main className='flex min-h-screen w-full items-center justify-center bg-[#f3f3f3] p-10 font-sans'>
@@ -12,7 +15,7 @@ export default function IndexPage() {
           className='text-center font-playFairDisplay text-xl font-bold tracking-[-0.1em]  text-black dark:text-white md:text-2xl'
           text={t('title')}
         />
-        <div className='flex justify-center gap-1'>
+        <div className='flex items-center justify-center gap-2'>
           <div className='cursor-pointer p-2 duration-300 hover:text-stone-500'>
             <a href='mailto: shuyuan010829@gmail.com'>
               <Mail />
@@ -28,6 +31,9 @@ export default function IndexPage() {
               <Linkedin />
             </a>
           </div>
+          <Link href={`/${locale}/resume`}>
+            <GetStartedButton text='See Resume' className='font-montserrat' />
+          </Link>
         </div>
         <div className='text-xs'>
           © <span className='font-bold'>2024</span> Shuyuan Chuang
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,3 @@
-import { Mail, Github, Linkedin } from 'lucide-react';
-
 export default function Home() {
-  return (
-    <main className='flex min-h-screen w-full items-center justify-center bg-[#f3f3f3] p-10 font-sans'>
-      <div className='flex flex-col items-center gap-5'>
-        <div className='text-2xl font-bold'>網站重新維修中...⸜(๑˙ᵕ ˙๑)⸝</div>
-        <div className='flex justify-center gap-1'>
-          <div className='cursor-pointer p-2 duration-300 hover:text-stone-500'>
-            <a href='mailto: shuyuan010829@gmail.com'>
-              <Mail />
-            </a>
-          </div>
-          <div className='cursor-pointer p-2 duration-300 hover:text-stone-500'>
-            <a href='https://github.com/SHUSHU010829' target='_blank'>
-              <Github />
-            </a>
-          </div>
-          <div className='cursor-pointer p-2 duration-300 hover:text-stone-500'>
-            <a href='https://www.linkedin.com/in/shuyuanchuang' target='_blank'>
-              <Linkedin />
-            </a>
-          </div>
-        </div>
-        <div className='text-xs'>
-          © <span className='font-bold'>2024</span> Shuyuan Chuang
-        </div>
-      </div>
-    </main>
-  );
+  return <></>;
 }
