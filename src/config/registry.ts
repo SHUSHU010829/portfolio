50ccefc5d7a32b909dd99d3a2aa2438b69a90fe8
SHUSHU <shushu90829@gmail.com>
2026-02-02 00:29:13 +0800

    clear

--- src/config/registry.ts
+++
@@ -1,6 +0,0 @@
-export const REGISTRY_CONFIG = {
-  namespace: process.env.REGISTRY_NAMESPACE || "@shuyuan",
-  namespaceUrl:
-    process.env.REGISTRY_NAMESPACE_URL || "https://shuyuan.dev/r/{name}.json",
-  baseUrl: process.env.APP_URL || "https://shuyuan.dev",
-};
